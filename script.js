92916e293492a09a84f9f2bf2f1e3e49550ad49e
Trauvin <mateusgalote@gmail.com>
2022-01-10 18:19:45 -0300

    campos de contato e comportamento com js adicionados

---
+++ script.js
@@ -0,0 +1,23 @@
+function changeColorInputOnClick() {
+  const inputs = document.querySelectorAll('input');
+
+  function changeColor(event) {
+    const label = event.target.previousElementSibling;
+
+    if (label.getAttribute('class') === 'on') {
+      label.classList.remove('on');
+    } else {
+      label.classList.add('on');
+    }
+
+    console.log(label)
+  }
+
+  inputs.forEach(input => {
+    ['focus', 'blur'].forEach(evento =>  {
+      input.addEventListener(evento, changeColor)
+    });
+  });
+}
+
+changeColorInputOnClick();
